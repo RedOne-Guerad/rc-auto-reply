88af371145cc2166632706d9e7f34c8c88f936d9
Redouane GUERAD <redouane.developer@gmail.com>
2023-10-11 22:33:03 +0100

    move src files

--- IAutoReplySettings.ts
+++
@@ -1,7 +0,0 @@
-import { IUser } from "@rocket.chat/apps-engine/definition/users";
-
-export interface IAutoReplySettings {
-    on: boolean;
-    message: string | 'Hey, I received your message and will get back to you as soon as possible.';
-    users?: IUser[] | undefined;
-  };
