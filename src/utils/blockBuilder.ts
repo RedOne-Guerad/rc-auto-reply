a9bde8c4a048a8f412c455e9be0f54c5204e6c59
Redouane GUERAD <redouane.developer@gmail.com>
2024-11-14 09:44:55 +0100

    add separat logic for block builder

---
+++ src/utils/blockBuilder.ts
@@ -0,0 +1,121 @@
+import { ButtonStyle } from "@rocket.chat/apps-engine/definition/uikit";
+import { ActionsBlock, ButtonElement, ContextBlock, DividerBlock, InputBlock, Option, SectionBlock, StaticSelectElement } from "@rocket.chat/ui-kit";
+import { uuid } from "./helpers";
+
+export function InputBlock(labelText: string, placeholderText: string, blockId: string, actionId: string, initialValue?: string, multiline?: boolean): InputBlock {
+  return {
+    type: "input",
+    label: {
+      type: "plain_text",
+      text: labelText,
+    },
+    element: {
+      type: "plain_text_input",
+      placeholder: {
+        type: "plain_text",
+        text: placeholderText,
+      },
+      appId: uuid(),
+      blockId: blockId,
+      actionId: actionId,
+      initialValue: initialValue,
+      multiline: multiline,
+    },
+  };
+}
+
+export function DateInputBlock(labelText: string, placeholderText: string, blockId: string, actionId: string, initialDate?: string): InputBlock {
+  return {
+    type: "input",
+    label: {
+      type: "plain_text",
+      text: labelText,
+    },
+    element: {
+      type: "datepicker",
+      placeholder: {
+        type: "plain_text",
+        text: placeholderText,
+      },
+      appId: uuid(),
+      blockId: blockId,
+      actionId: actionId,
+      initialDate: initialDate,
+    },
+  };
+}
+
+export function Button(labelText: string, blockId: string, actionId: string, value?: string, style?: ButtonStyle.PRIMARY | ButtonStyle.DANGER, url?: string): ButtonElement {
+  return {
+    type: "button",
+    text: {
+      type: "plain_text",
+      text: labelText,
+    },
+    appId: uuid(),
+    blockId: blockId,
+    actionId: actionId,
+    url: url,
+    value: value,
+    style: style,
+  };
+}
+
+export function SectionBlock(labelText: string, accessory?: any): SectionBlock {
+  return {
+    type: "section",
+    text: {
+      type: "plain_text",
+      text: labelText,
+    },
+    accessory: accessory,
+  };
+}
+
+export function DividerBlock(): DividerBlock {
+  return {
+    type: "divider",
+  };
+}
+
+export function ContextBlock(elementText: string): ContextBlock {
+  return {
+    type: "context",
+    elements: [
+      {
+        type: "plain_text",
+        text: elementText,
+      },
+    ],
+  };
+}
+
+export function StaticSelectElement(placeholderText: string, options: Array<Option>, blockId: string, actionId: string, initialValue?: Option["value"]): StaticSelectElement {
+  return {
+    type: "static_select",
+    placeholder: {
+      type: "plain_text",
+      text: placeholderText,
+    },
+    options: options,
+    appId: uuid(),
+    blockId: blockId,
+    actionId: actionId,
+    initialValue: initialValue,
+  };
+}
+
+export function Option(text: string, value: string): Option {
+  return {
+    text: { type: "plain_text", text: text },
+    value: value,
+  };
+}
+
+export function ActionsBlock(blockId: string, elements: Array<ButtonElement | StaticSelectElement>): ActionsBlock {
+  return {
+    type: "actions",
+    blockId: blockId,
+    elements: elements,
+  };
+}
